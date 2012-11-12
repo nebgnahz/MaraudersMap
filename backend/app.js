e8599b3cf8e4a98ddc57a8b8c1a91be34a3a27ac
Ben Zhang <benzh@eecs.berkeley.edu>
2012-11-12 10:27:21 -0800

    added image file and enabled node for .gif response

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,11 @@
 var http = require('http')
 , app = require('http').createServer(handler)
 , io = require('socket.io').listen(app)
-, fs = require('fs');
+, url = require('url')
+, fs = require('fs')
+, querystring = require('querystring')
+, util = require('util');
+
 
 app.listen(8888);
 
@@ -14,19 +18,6 @@ var options = {
     path: '/republish',
     method: 'POST'
 };
-
-// function that handles incoming HTTP request
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-    res.writeHead(200);
-    res.end(data);
-  });
-}
 
 // when new connection established, start query
 io.sockets.on('connection', function (socket) {
@@ -46,3 +37,67 @@ io.sockets.on('connection', function (socket) {
     console.log("waiting for sMAP responses");
     req.end();
 });
+
+// this is the RESTful server part
+function handler (req, res) {
+    // your normal server code
+    console.log(req.url);
+    var path = url.parse(req.url).pathname;
+    var query = querystring.parse(url.parse(req.url).query);
+    switch (path){
+    case '/':
+	fs.readFile(__dirname + '/index.html',
+		    function (err, data) {
+			if (err) {
+			    res.writeHead(500);
+			    return res.end('Error loading index.html');
+			}
+			res.writeHead(200);
+			res.end(data);
+		    });
+	break;
+    case '/favicon.ico':
+	break;
+    default:
+	if (/\.(js|html|swf)$/.test(path)){
+	    try {
+		var swf = path.substr(-4) === '.swf';
+		res.writeHead(200, {'Content-Type': swf ? 'application/x-shockwave-flash' : ('text/' + (path.substr(-3) === '.js' ? 'javascript' : 'html'))});
+		res.write(fs.readFileSync(__dirname + path, swf ? 'binary' : 'utf8'), swf ? 'binary' : 'utf8');
+		res.end();
+	    } catch(e){ 
+		res.writeHead(500);
+		return res.end('Error loading index.html');
+	    }               
+	    break;
+	}
+	else if (/\.(css)$/.test(path)){
+	    res.writeHead(200, {'Content-Type': 'text/css'});
+	    res.write(fs.readFileSync(__dirname + path, 'utf8'));
+	    res.end();
+	    break;
+	}
+	else if (/\.(gif)$/.test(path)){
+	    fs.stat(__dirname+path, function(error, stat) {
+		var rs;
+		// We specify the content-type and the content-length headers
+		// important!
+		res.writeHead(200, {
+		    'Content-Type' : 'image/gif',
+		    'Content-Length' : stat.size
+		});
+		rs = fs.createReadStream(__dirname + path);
+		// pump the file to the response
+		util.pump(rs, res, function(err) {
+		    if(err) {
+			throw err;
+		    }
+		});
+	    });
+	    break;
+	}
+	res.writeHead(500);
+	return res.end('Error loading index.html');
+	break;
+    }
+}
